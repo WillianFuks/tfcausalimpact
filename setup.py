3681cbaefd967b08cfc00621f82bd0adadaaf28e
Willian Fuks <willian.fuks@gmail.com>
2020-12-16 14:19:25 -0300

    add .travis.yml (#2)
    * Added .travis.yml
    
    * Removed py39 support
    
    * Bionic dist
    
    * Fix unit tests

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ if sys.argv[-1] == 'publish':
 install_requires = [
     'jinja2',
     'pandas',
-    'tensorflow>=2.3.0',
-    'tensorflow-probability>=0.11.1',
+    'tensorflow',
+    'tensorflow-probability',
     'numpy',
     'scipy',
     'matplotlib',
@@ -94,7 +94,6 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Scientific/Engineering',
     ],
