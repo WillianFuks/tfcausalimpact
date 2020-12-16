a82d0f5a1479e00fd1ce96a3ede8645bb3f3123d
WillianFuks <willian.fuks@gmail.com>
2020-12-16 11:18:22 -0300

    remove py39 support

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ install_requires = [
     'jinja2',
     'pandas',
     'tensorflow>=2.3.0',
-    'tensorflow-probability>=0.11.1',
+    'tensorflow-probability>=0.11.0',
     'numpy',
     'scipy',
     'matplotlib',
